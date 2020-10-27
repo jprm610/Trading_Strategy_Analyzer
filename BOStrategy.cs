4fe159450673b145c98b18e4f516fb6d4ebc49e8
jprm610 <63762478+jprm610@users.noreply.github.com>
2020-10-26 19:14:01 -0500

    V99 (#6)
    * 0.1
    
    Prints rectangles for the last 10 heat zones.
    
    * 0.2
    
    instance parameter added, changed to draw ranges instead of rectangles.
    
    * 0.3
    
    Draws rectangles correctly but only for swingHighs.
    
    * 0.4
    
    Heat zones based on swing lows and swing highs.
    
    * 0.5
    
    Heat zones that counts lows and highs.
    
    * 0.6
    
    Lists_Cleaning(). Has bugsgit add HeatZones.csgit add HeatZones.cs.
    
    * 0.7
    
    Prints the last 10 swings. Determines which swings are in the current heat zone. MySwing class.
    
    * 0.8
    
    Corrected swing lrecognition issues.
    
    * 0.9
    
    Takes into account the current swings in order to draw the rectangle.
    
    * 1.0
    
    Heat_zone_strength parameter fixed. Heat_zone_print new parameter. Prints heat_zone.strength in screen.
    
    * 1.1
    
    Width parameter added. Phase 1 finished.
    
    * 1.2
    
    Detects heat zones without differing into low and high.
    
    * 1.3
    
    Prints the reference value in screen. Clean code.
    
    * 1.4
    
    Comments added.
    
    * 1.5
    
    Parameters problems solved.
    
    * 1.6
    
    Attached the Heat-Zones process to BOStrategy. Parameters ordered in 2 other groups. Changed Heat Zones visual format.
    
    * 1.7
    
    Erased the HeatZones.cs file.

--- BOStrategy.cs
+++ BOStrategy.cs
@@ -37,8 +37,17 @@ namespace NinjaTrader.NinjaScript.Strategies
 		private double ATR_crossing_value, SMA_dis, stop_price_long, trigger_price_long, stop_price_short, trigger_price_short, swingHigh2_max, swingLow2_min, MaxCloseSwingLow4, RangeSizeSwingLow4, MaxCloseSwingLow14;
 		private double last_max_high_swingHigh1, last_max_high_swingHigh2, last_min_low_swingLow1, last_min_low_swingLow2, distance_to_BO;
 		private double RangeSizeSwingLow14, MinCloseSwingHigh4, RangeSizeSwingHigh4, MinCloseSwingHigh14, RangeSizeSwingHigh14, swingHigh2_max_reentry, swingLow2_min_reentry, max_high_swingHigh1, max_high_swingHigh2, min_low_swingLow1, min_low_swingLow2;
-		private double current_swingHigh2, current_swingLow2, current_swingHigh1, current_swingLow1, current_ATR, current_stop, CurrentOpen, CurrentClose, CurrentHigh, CurrentLow, fix_stop_price_long, fix_trigger_price_long, fix_stop_price_short, fix_trigger_price_short;
+		private double last_swingHigh2, last_swingLow2, last_swingHigh1, last_swingLow1, current_ATR, current_stop, CurrentOpen, CurrentClose, CurrentHigh, CurrentLow, fix_stop_price_long, fix_trigger_price_long, fix_stop_price_short, fix_trigger_price_short;
 		private bool is_incipient_up_trend, is_incipient_down_trend, is_upward, is_downward, gray_ellipse_long, gray_ellipse_short, is_reentry_long, is_reentry_short, is_long, is_short, isBO, is_BO_up_swing1, is_BO_up_swing2, is_BO_down_swing1, is_BO_down_swing2;
+
+		#region Heat_Zones_Process_Variables
+		private double heat_zone_reference_value, swingDis, max_swing, min_swing;
+		List<MySwing> swings2_high = new List<MySwing>();
+		List<MySwing> swings2_low = new List<MySwing>();
+		List<MySwing> heat_zone_swings2 = new List<MySwing>();
+		List<MyHeatZone> heat_zones = new List<MyHeatZone>();
+		#endregion
+
 		private bool cross_below_iSMA3_to_iSMA2, cross_above_iSMA3_to_iSMA2;
 		private Account myAccount;
 		private Order my_entry_order = null, my_entry_market = null, my_exit_order = null;
@@ -56,7 +65,7 @@ namespace NinjaTrader.NinjaScript.Strategies
 				IsExitOnSessionCloseStrategy	= false;
 				ExitOnSessionCloseSeconds		= 30;
 				IsFillLimitOnTouch				= false;
-				MaximumBarsLookBack				= MaximumBarsLookBack.TwoHundredFiftySix;
+				MaximumBarsLookBack				= MaximumBarsLookBack.Infinite;
 				OrderFillResolution				= OrderFillResolution.Standard;
 				Slippage						= 0;
 				StartBehavior					= StartBehavior.WaitUntilFlat;
@@ -85,6 +94,10 @@ namespace NinjaTrader.NinjaScript.Strategies
 				ClosnessFactor			= 2;
 				ClosnessToTrade			= 1;
 				MagicNumber				= 50;
+				Instance				= 10;
+				Heat_zone_strength		= 2;
+				Width					= 1.5;
+				Heat_zones_print		= 6;
 			}
 			else if (State == State.Configure)
 			{
@@ -198,24 +211,40 @@ namespace NinjaTrader.NinjaScript.Strategies
 				return;
 
 			//This conditional checks that the indicator values that will be used in later calculations are not equal to 0.
-			if (iSwing2.SwingHigh[0] == 0 || iSwing2.SwingLow[0] == 0 || iSwing1.SwingHigh[0] == 0 || iSwing1.SwingLow[0] == 0 || iATR[0] == 0)
+			if (iSwing1.SwingHigh[0] == 0 || iSwing1.SwingLow[0] == 0 ||
+				iSwing2.SwingHigh[0] == 0 || iSwing2.SwingLow[0] == 0 || 
+				iATR[0] == 0)
 				return;
-            #endregion
 
-            #region Variable_Reset
+			//Review that the swings are charged to avoid later bugs. In other words, to check if the needed swing instances exist.
+			for (int i = 1; i <= Instance; i++)
+			{
+				if (iSwing2.SwingHighBar(0, i, CurrentBar) == -1)
+					return;
 
-            //Reset is_BO variable once a new swing comes up.
-            #region BO_reset
-            if (current_swingHigh1 != iSwing1.SwingHigh[0])
+				if (iSwing2.SwingLowBar(0, i, CurrentBar) == -1)
+					return;
+			}
+
+			//Reset the swings that are going to be evaluated in each Heat Zone claculation.
+			//This has to be done because every Heat Zone has different Reference Values.
+			heat_zone_swings2.Clear();
+			#endregion
+
+			#region Variable_Reset
+
+			//Reset is_BO variable once a new swing comes up.
+			#region BO_reset
+			if (last_swingHigh1 != iSwing1.SwingHigh[0])
 				is_BO_up_swing1 = false;
 
-			if (current_swingHigh2 != iSwing2.SwingHigh[0])
+			if (last_swingHigh2 != iSwing2.SwingHigh[0])
 				is_BO_up_swing2 = false;
 
-			if (current_swingLow1 != iSwing1.SwingLow[0])
+			if (last_swingLow1 != iSwing1.SwingLow[0])
 				is_BO_down_swing1 = false;
 
-			if (current_swingLow2 != iSwing2.SwingLow[0])
+			if (last_swingLow2 != iSwing2.SwingLow[0])
 				is_BO_down_swing2 = false;
 			#endregion
 
@@ -235,11 +264,7 @@ namespace NinjaTrader.NinjaScript.Strategies
 			is_reentry_short = false;
 			#endregion
 
-			#region Variable_Initialization       
-			current_swingHigh1 = iSwing1.SwingHigh[0];
-			current_swingLow1 = iSwing1.SwingLow[0];
-			current_swingHigh2 = iSwing2.SwingHigh[0];
-			current_swingLow2 = iSwing2.SwingLow[0];
+			#region Variable_Initialization
 			current_ATR = iATR[0];
 			current_stop = current_ATR * ATRStopFactor;
 			CurrentOpen = Open[0];
@@ -347,6 +372,207 @@ namespace NinjaTrader.NinjaScript.Strategies
 
 				Print(string.Format("The Swings have been sorted as follow: iSwing1: {0} // iSwing2: {1}.", Swing1, Swing2));
 			}
+			#endregion
+
+			#region Heat_Zones_Process
+			#region Save_Swings
+			//In each swing update, save the last n swings highs and lows, whit its corresponding CurrentBar. (n = Instance)
+			//This swings are saved in a list (swings2_high and swings2_low) of class MySwing which is defined in the classes region.
+			if (last_swingHigh2 != iSwing2.SwingHigh[0] || last_swingLow2 != iSwing2.SwingLow[0])
+			{
+				for (int i = 1; i <= Instance; i++)
+				{
+					swings2_high.Add(new MySwing()
+					{
+						value = iSwing2.SwingHigh[iSwing2.SwingHighBar(0, i, CurrentBar)],
+						bar = CurrentBar - iSwing2.SwingHighBar(0, i, CurrentBar)
+					});
+				}
+
+				while (swings2_high.Count > Instance)
+				{
+					swings2_high.RemoveAt(0);
+				}
+
+				for (int i = 1; i <= Instance; i++)
+				{
+					swings2_low.Add(new MySwing()
+					{
+						value = iSwing2.SwingLow[iSwing2.SwingLowBar(0, i, CurrentBar)],
+						bar = CurrentBar - iSwing2.SwingLowBar(0, i, CurrentBar)
+					});
+				}
+
+				while (swings2_low.Count > Instance)
+				{
+					swings2_low.RemoveAt(0);
+				}
+			}
+			#endregion
+
+			#region Reference_Value_Definition
+			//Determine the reference current Heat Zone value in each swing update.
+			//If a swingHigh appears, it is going to be taken as the current reference value. The same happens with swings low.
+			if (last_swingHigh2 != iSwing2.SwingHigh[0])
+				heat_zone_reference_value = swings2_high[0].value;
+
+			if (last_swingLow2 != iSwing2.SwingLow[0])
+				heat_zone_reference_value = swings2_low[0].value;
+			#endregion
+
+			#region Swing_Heat
+			//In each swing update, determine which of the saved swings are located in the current Heat Zone range.
+			//If the swing (low or high) is in range, its value and CurrentBar is saved in another list (heat_zone_swings2) of class MySwing.
+			if (last_swingHigh2 != iSwing2.SwingHigh[0] || last_swingLow2 != iSwing2.SwingLow[0])
+			{
+				for (int i = 0; i < swings2_high.Count - 1; i++)
+				{
+					swingDis = Math.Abs(heat_zone_reference_value - swings2_high[i].value);
+
+					if (swingDis <= iATR[0] * Width)
+					{
+						heat_zone_swings2.Add(new MySwing()
+						{
+							value = swings2_high[i].value,
+							bar = swings2_high[i].bar
+						});
+					}
+				}
+
+				for (int i = 0; i < swings2_low.Count - 1; i++)
+				{
+					swingDis = Math.Abs(heat_zone_reference_value - swings2_low[i].value);
+
+					if (swingDis <= iATR[0] * Width)
+					{
+						heat_zone_swings2.Add(new MySwing()
+						{
+							value = swings2_low[i].value,
+							bar = swings2_low[i].bar
+						});
+					}
+				}
+
+				while (heat_zone_swings2.Count > Instance)
+				{
+					heat_zone_swings2.RemoveAt(0);
+				}
+			}
+			#endregion
+
+			#region Heat_Zone_Identification
+			//Deetermine if the current Heat Zone satisfies the heat_zone_strength requirement.
+			//In other words, save the current Heat Zone (in a list of class MyHeatZone),
+			//if it has the same or more number of swings in the range.
+			if (heat_zone_swings2.Count - 1 >= Heat_zone_strength)
+			{
+				//Initialize Heat Zones characteristics.
+				//max_y, min_y and start_x are going to be established correctly in the next for loop.
+				heat_zones.Add(new MyHeatZone()
+				{
+					reference_value = heat_zone_reference_value,
+					max_y = heat_zone_swings2[0].value,
+					min_y = heat_zone_swings2[0].value,
+					start_x = heat_zone_swings2[0].bar,
+					strength = heat_zone_swings2.Count - 1
+				});
+
+				//Determine the dimensions of the rectangle that will be printed as a Heat Zone. 
+				for (int i = 0; i < heat_zone_swings2.Count; i++)
+				{
+					if (heat_zone_swings2[i].value > heat_zones[heat_zones.Count - 1].max_y)
+					{
+						heat_zones[heat_zones.Count - 1].max_y = heat_zone_swings2[i].value;
+					}
+
+					if (heat_zone_swings2[i].value < heat_zones[heat_zones.Count - 1].min_y)
+					{
+						heat_zones[heat_zones.Count - 1].min_y = heat_zone_swings2[i].value;
+					}
+
+					if (heat_zone_swings2[i].bar < heat_zones[heat_zones.Count - 1].start_x)
+					{
+						heat_zones[heat_zones.Count - 1].start_x = heat_zone_swings2[i].bar;
+					}
+				}
+			}
+
+			//Avoid that a Heat Zone is printed over another one.
+			//Evaluating if its Start_x or its reference value is the same. 
+			for (int i = 0; i < heat_zones.Count; i++)
+			{
+				for (int j = 0; j < heat_zones.Count; j++)
+				{
+					if (i != j)
+					{
+						if (heat_zones[i].start_x == heat_zones[j].start_x)
+						{
+							heat_zones.RemoveAt(i);
+						}
+						//If its reference value is the same. Erase the oldest of both Heat Zones.
+						else if (heat_zones[i].reference_value == heat_zones[j].reference_value)
+						{
+							if (heat_zones[i].start_x < heat_zones[j].start_x)
+							{
+								heat_zones.RemoveAt(i);
+							}
+							else
+							{
+								heat_zones.RemoveAt(j);
+							}
+						}
+					}
+				}
+			}
+
+			//Erase old Heat Zones.
+			while (heat_zones.Count > Heat_zones_print)
+			{
+				heat_zones.RemoveAt(0);
+			}
+			#endregion
+
+			#region Extreme_Swings
+			//Determine the extreme swings of the last n Instances.
+			//Then plot lines at that values.
+
+			max_swing = iSwing2.SwingHigh[0];
+			for (int i = 1; i < Instance; i++)
+			{
+				if (iSwing2.SwingHigh[iSwing2.SwingHighBar(0, i, CurrentBar)] > max_swing)
+				{
+					max_swing = iSwing2.SwingHigh[iSwing2.SwingHighBar(0, i, CurrentBar)];
+				}
+			}
+			Draw.HorizontalLine(this, "MaxLine", max_swing, Brushes.LimeGreen, DashStyleHelper.Solid, 5);
+
+			min_swing = iSwing2.SwingLow[0];
+			for (int i = 1; i < Instance; i++)
+			{
+				if (iSwing2.SwingLow[iSwing2.SwingLowBar(0, i, CurrentBar)] < min_swing)
+				{
+					min_swing = iSwing2.SwingLow[iSwing2.SwingLowBar(0, i, CurrentBar)];
+				}
+			}
+			Draw.HorizontalLine(this, "MinLine", min_swing, Brushes.LimeGreen, DashStyleHelper.Solid, 5);
+			#endregion
+
+			#region Print_Heat_Zones
+			//Prints rectangles that represents the Heat Zones, the strength ot the Heat Zone and its reference value.
+			//The rectangle dimenssions were set in the Heat_Zone_Identification region.
+			for (int i = 0; i < heat_zones.Count; i++)
+			{
+				Draw.Rectangle(this, "Rectangle" + i, true, CurrentBar - heat_zones[i].start_x, heat_zones[i].min_y, 0, heat_zones[i].max_y, Brushes.Orange, BackBrush, 0);
+				Draw.Text(this, "Strength" + i, heat_zones[i].strength.ToString(), 10, (heat_zones[i].max_y + heat_zones[i].min_y) / 2);
+				Draw.Text(this, "RV" + i, heat_zones[i].reference_value.ToString(), 30, (heat_zones[i].max_y + heat_zones[i].min_y) / 2);
+			}
+			#endregion
+
+			//Reset the last swing value in order to execute the swing update processes.
+			last_swingHigh1 = iSwing2.SwingHigh[0];
+			last_swingLow1 = iSwing2.SwingLow[0];
+			last_swingHigh2 = iSwing2.SwingHigh[0];
+			last_swingLow2 = iSwing2.SwingLow[0];
 			#endregion
 
 			#region Overall_Market_Movement
@@ -1417,102 +1643,145 @@ namespace NinjaTrader.NinjaScript.Strategies
 			#endregion
 		}
 
-		#region Properties
-		[NinjaScriptProperty]
+        #region Properties
+        #region Indicators
+        [NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "SMA1 (Max)", Order = 1, GroupName = "Parameters")]
+		[Display(Name = "SMA1 (Max)", Order = 1, GroupName = "Indicators")]
 		public int SMA1
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "SMA2 (Mid)", Order = 2, GroupName = "Parameters")]
+		[Display(Name = "SMA2 (Mid)", Order = 2, GroupName = "Indicators")]
 		public int SMA2
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "SMA3 (Min)", Order = 3, GroupName = "Parameters")]
+		[Display(Name = "SMA3 (Min)", Order = 3, GroupName = "Indicators")]
 		public int SMA3
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "ATR1", Order = 4, GroupName = "Parameters")]
+		[Display(Name = "ATR1", Order = 4, GroupName = "Indicators")]
 		public int ATR1
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "Swing1 (Min)", Order = 5, GroupName = "Parameters")]
+		[Display(Name = "Swing1 (Min)", Order = 5, GroupName = "Indicators")]
 		public int Swing1
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "Swing2 (Max)", Order = 6, GroupName = "Parameters")]
+		[Display(Name = "Swing2 (Max)", Order = 6, GroupName = "Indicators")]
 		public int Swing2
 		{ get; set; }
+        #endregion
 
-		[NinjaScriptProperty]
+        #region Parameters
+        [NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "TrailingUnitsStop", Order = 7, GroupName = "Parameters")]
+		[Display(Name = "TrailingUnitsStop", Order = 1, GroupName = "Parameters")]
 		public double TrailingUnitsStop
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "UnitsTriggerForTrailing", Order = 8, GroupName = "Parameters")]
+		[Display(Name = "UnitsTriggerForTrailing", Order = 2, GroupName = "Parameters")]
 		public double UnitsTriggerForTrailing
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name = "RiskUnit", Order = 9, GroupName = "Parameters")]
+		[Display(Name = "RiskUnit", Order = 3, GroupName = "Parameters")]
 		public int RiskUnit
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "IncipientTrandFactor", Order = 10, GroupName = "Parameters")]
+		[Display(Name = "IncipientTrandFactor", Order = 4, GroupName = "Parameters")]
 		public double IncipientTrandFactor
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "ATRStopFactor", Order = 11, GroupName = "Parameters")]
+		[Display(Name = "ATRStopFactor", Order = 5, GroupName = "Parameters")]
 		public double ATRStopFactor
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "TicksToBO", Order = 12, GroupName = "Parameters")]
+		[Display(Name = "TicksToBO", Order = 6, GroupName = "Parameters")]
 		public double TicksToBO
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, 100)]
-		[Display(Name = "SwingPenetration(%)", Order = 13, GroupName = "Parameters")]
+		[Display(Name = "SwingPenetration(%)", Order = 7, GroupName = "Parameters")]
 		public double SwingPenetration
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "ClosnessFactor", Order = 14, GroupName = "Parameters")]
+		[Display(Name = "ClosnessFactor", Order = 8, GroupName = "Parameters")]
 		public double ClosnessFactor
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name = "ClosnessToTrade", Order = 15, GroupName = "Parameters")]
+		[Display(Name = "ClosnessToTrade", Order = 9, GroupName = "Parameters")]
 		public double ClosnessToTrade
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(0, 100)]
-		[Display(Name = "MagicNumber(Percent (%) of ATR)", Order = 16, GroupName = "Parameters")]
+		[Display(Name = "MagicNumber(Percent (%) of ATR)", Order = 10, GroupName = "Parameters")]
 		public double MagicNumber
 		{ get; set; }
+        #endregion
+
+        #region Heat_Zone
+        [NinjaScriptProperty]
+		[Display(Name = "Instance", GroupName = "Heat Zone", Order = 1)]
+		public int Instance
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Display(Name = "Heat_zone_strength", GroupName = "Heat Zone", Order = 2)]
+		public int Heat_zone_strength
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Display(Name = "Width", GroupName = "Heat Zone", Order = 3)]
+		public double Width
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Display(Name = "Heat_zones_print", GroupName = "Heat Zone", Order = 4)]
+		public int Heat_zones_print
+		{ get; set; }
+        #endregion
+        #endregion
+
+        #region Classes
+        public class MySwing
+		{
+			public double value;
+			public int bar;
+		}
+
+		public class MyHeatZone
+		{
+			public double reference_value;
+			public double max_y;
+			public double min_y;
+			public int start_x;
+			public int strength;
+		}
 		#endregion
 
 		#region Functions
