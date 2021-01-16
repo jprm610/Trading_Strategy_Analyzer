7fd6567c798bee81b7c605fb3b195ebcd757ede4
Juan Pablo Robledo <jprm610@gmail.com>
2021-01-15 19:31:45 -0500

    0.4
    Added the SMA and ATR indicators functions. Plots dataframe with all values.

--- Model/indicators.py
+++ Model/indicators.py
@@ -5,15 +5,51 @@ class holder :
     1
 
 #Simple Moving Average (SMA)
-def SMA(prices, periods) :
+def SMA(prices, period) :
 
-    results = holder()
+    #Create the list of moving averages results
+    moving_avg = []
 
-    moving_avg = {}
+    #For every row:
+    for i in range(0, len(prices)) :
+        #If there isn't enough data to calculate,
+        #set the current average to 0.
+        if i < period :
+            moving_avg.append(0)
+        #If there is:
+        else :
+            #Add all the last (period) closes and average them,
+            #then save it in the averages list.
+            sum = 0
+            for j in range(0, period) :
+                sum += prices.close[i - j - 1]
+            moving_avg.append(sum / period)
 
-    moving_avg = prices.close.rolling(center=False, window=periods)
+    return moving_avg
 
-    results = moving_avg
-    
-    return results
+#Average True Range (ATR)
+def ATR(prices, period) :
 
+    #Calculate all the ranges (high - close)
+    #and save it in the ranges list.
+    ranges = []
+    for i in range(0, len(prices)) :
+        ranges.append(prices.high[i] - prices.low[i])
+
+    #For every row:
+    ATRs = []
+    for i in range(0, len(prices)) :
+        #If there isn't enough data to calculate,
+        #set the current average to 0.
+        if i < period :
+            ATRs.append(0)
+        #If there is:
+        else :
+            #Add all the last (period) ranges and average them,
+            #then save it in the ATRs list.
+            sum = 0
+            for j in range(0, period) :
+                sum += ranges[i - j - 1]
+            ATRs.append(sum / period)
+
+    return ATRs
--- Model/main.py
+++ Model/main.py
@@ -8,8 +8,33 @@ df = pd.read_csv("EURUSD.csv")
 #Rename df columns for cleaning porpuses
 df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
 
-#df.date = pd.to_datetime(df.date, format='%d. %m. %Y %H:%M:%S:%f')
+#Reformat the date column
+df.date = pd.to_datetime(df.date)
 
-df.set_index(df.date)
+#Set the date column as the index
+df = df.set_index(df.date)
 
-SMA = SMA(df, 50)
+#Delete the old date column
+del df['date']
+
+#Drop duplicates leaving only the first value
+df = df.drop_duplicates(subset = "close", keep = "first")
+
+print(df.head())
+
+#Get all indicator lists
+SMA20  = SMA(df, 20)
+SMA50  = SMA(df, 50)
+SMA200 = SMA(df, 200)
+ATR = ATR(df, 100)
+
+#Attach those lists to columns
+df['SMA20']  = np.array(SMA20)
+df['SMA50']  = np.array(SMA50)
+df['SMA200'] = np.array(SMA200)
+df['ATR100'] = np.array(ATR)
+
+print(df.head(20))
+
+#Save the edited dataframe as a new .csv file
+df.to_csv('Final_frame.csv')
