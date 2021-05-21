3f94ee760277dd71ae08f124e4dd15048fa6832f
jprm610 <jprm610@gmail.com>
2021-05-21 16:09:03 -0500

    2.6

--- Model/SP_briefcase.py
+++ Model/SP_briefcase.py
@@ -38,6 +38,7 @@ trades_global = pd.DataFrame()
 # region PARAMETERS
 risk_unit = 100
 perc_in_risk = 4
+trades_at_the_same_time = 10
 # endregion
 
 #tickers = tickers[0:10].copy()
@@ -247,6 +248,74 @@ for asset in tickers :
 # Here the trades_global df is edited 
 # in order to set the entry_date characteristic as the index.
 trades_global = trades_global.sort_values(by=['entry_date'])
+
+# region Portfolio
+
+# region Number of Trades DF
+
+# Here is built a df with the number of trades by every date,
+# in order to track how many trades were executed in every date.
+Trading_dates = []
+Number_Trades_per_Date = []
+Number_Closed_Trades_per_Date = []
+Trades_per_date = 0
+last_date = trades_global['entry_date'].values[0]
+for i in range(len(trades_global)) :
+    if trades_global['entry_date'].values[i] == last_date :
+        Trades_per_date += 1
+    else : 
+        Closed_Trades_per_Date = sum(1 for x in trades_global['exit_date'][0:i-1] if x <= last_date) - sum(Number_Closed_Trades_per_Date)
+        Trading_dates.append(last_date)
+        Number_Trades_per_Date.append(Trades_per_date)
+        Number_Closed_Trades_per_Date.append(Closed_Trades_per_Date)
+        last_date = trades_global['entry_date'].values[i]
+        Trades_per_date = 1
+
+Number_of_trades = pd.DataFrame()
+Number_of_trades['date'] = np.array(Trading_dates)
+Number_of_trades['# of trades'] = np.array(Number_Trades_per_Date)
+Number_of_trades['# of closed trades'] = np.array(Number_Closed_Trades_per_Date)
+Number_of_trades.set_index(Number_of_trades['date'], drop=True, inplace=True)
+del Number_of_trades['date']
+
+# endregion
+
+# region Clean DF
+
+# Here is built a df just with the trades that can be entered
+# taking into account the portfolio rules and the Number_of_trades df.
+Portfolio_Trades = pd.DataFrame()
+Newest_closes = [0]
+Closed_Trades_per_Date = 0
+Acum_Opened = Number_of_trades['# of trades'].values[0]
+Acum_Total = 0
+Acum_Closed = 0
+Counter = 0
+Slots = trades_at_the_same_time
+for i in range(len(Number_of_trades)) :
+    if i != 0 :
+        Acum_Closed = sum(1 for x in Portfolio_Trades['exit_date'] if x <= Number_of_trades.index.values[i])
+        Closed_Trades_per_Date = Acum_Closed - sum(Newest_closes)
+        Newest_closes.append(Closed_Trades_per_Date)
+        Acum_Opened = sum(1 for x in Portfolio_Trades['entry_date'] if x == Number_of_trades.index.values[i-1]) + Acum_Opened
+
+    Counter = Acum_Total - Closed_Trades_per_Date
+    Acum_Total = Acum_Opened - Acum_Closed
+    if Counter <= Slots :
+        if Number_of_trades['# of trades'].values[i] > Slots - Counter :
+            Filtered_Trades = pd.DataFrame()
+            Filtered_Trades = Filtered_Trades.append(trades_global[trades_global['entry_date'] == Number_of_trades.index.values[i]], ignore_index=True)
+            Filtered_Trades = Filtered_Trades.sort_values(by=['iRSI'])
+            Portfolio_Trades = Portfolio_Trades.append(Filtered_Trades[0:Slots - Counter], ignore_index=True)
+        else :
+            Portfolio_Trades = Portfolio_Trades.append(trades_global[trades_global['entry_date'] == Number_of_trades.index.values[i]], ignore_index=True)
+
+# endregion
+
+trades_global = Portfolio_Trades
+
+# endregion
+
 trades_global.set_index(trades_global['entry_date'], drop=True, inplace=True)
 del trades_global['entry_date']
 
