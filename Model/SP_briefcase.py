e0afbb8546997af72a28cb0133df07b4f95917cf
jprm610 <jprm610@gmail.com>
2021-05-27 19:32:12 -0500

    8.9

--- Model/SP_briefcase.py
+++ Model/SP_briefcase.py
@@ -542,6 +542,36 @@ trades_global = Portfolio_Trades
 
 # endregion
 
+# region Return Table
+
+# Here the trades_global df is edited 
+# in order to sart the df by exit_date.
+trades_global['exit_date'] =  pd.to_datetime(trades_global['exit_date'], format='%d/%m/%Y')
+trades_global = trades_global.sort_values(by=['exit_date'])
+trades_global.set_index(trades_global['entry_date'], drop=True, inplace=True)
+
+# Here is built the return_table df with the number of trades by every date.
+Profit_perc = []
+for i in range(len(trades_global)) :
+    Profit_perc.append(round(((trades_global['y_raw'].values[i] / trades_global['entry_price'].values[i]) / 10) * 100, 2))
+trades_global['Profit_perc'] = np.array(Profit_perc)
+trades_global['year'] = pd.DatetimeIndex(trades_global['exit_date']).year
+trades_global['month'] = pd.DatetimeIndex(trades_global['exit_date']).month
+
+return_table = pd.DataFrame()
+df1 = pd.DataFrame()
+return_table = trades_global.groupby([trades_global['year'], trades_global['month']])['Profit_perc'].sum().unstack(fill_value=0)
+df1 = return_table/100 + 1
+df1.columns = return_table.columns.map(str)
+return_table['Y%'] = round(((df1['1'] * df1['2'] * df1['3'] * df1['4'] * df1['5'] * df1['6'] *df1['7'] * df1['8'] * df1['9'] * df1['10'] * df1['11'] * df1['12'])-1)*100,2)
+return_table.to_csv('Return Table.csv', sep=';')
+
+del trades_global['Profit_perc']
+del trades_global['year']
+del trades_global['month']
+
+# endregion
+
 trades_global.set_index(trades_global['entry_date'], inplace=True)
 del trades_global['entry_date']
 
